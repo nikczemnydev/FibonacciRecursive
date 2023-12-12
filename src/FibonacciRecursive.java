7a3b750de3ac3cde910cbfc6b8ca81c462a6e8d8
Rafal <136376818+nikczemnydev@users.noreply.github.com>
2023-12-12 22:05:05 +0100

    Added a few comments that explain some steps.

--- src/FibonacciRecursive.java
+++ src/FibonacciRecursive.java
@@ -1,9 +1,11 @@
 public class FibonacciRecursive {
+    // initializing the values needed for calculations
     static int a = 0;
     static int b = 1;
     static int nextInSeq = 0;
     static int seqLimit = 1000000;
 
+    // main method calling the recursive method
     public static void main(String[] args) {
         System.out.println(a + " " + b);
 
